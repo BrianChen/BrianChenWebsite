7cfcee26017589836410f88e6dda2cd2492962b6
brian <brianc.cb@gmail.com>
2017-03-29 16:15:34 -0400

    spelling errors

--- lib/about.jsx
+++ lib/about.jsx
@@ -11,11 +11,11 @@ class About extends React.Component {
       <div id="about" className="sections">
         <h1>My journey as a Software Engineer</h1>
         <div className="about-content">
-          <p>I graduated from University at Buffalo with an Electrical Engineering degree.  After graduating I got a job as a Technical Support Engineer for a enterprise start up to learn more about software all while teaching myself programming such as Ruby and JavaScript.</p>
+          <p>I graduated from University at Buffalo with an Electrical Engineering degree.  After graduating worked as a Technical Support Engineer for a enterprise start up to learn more about software all while teaching myself programming languages such as Ruby and JavaScript.</p>
 
-          <p>After working for a year, I decided to purse software development full time.  I attended App Academy, an intensive coding school.</p>
+          <p>After working for a year, I decided to pursue software development full time.  I attended App Academy, an intensive coding school.</p>
 
-          <p>I enjoy working on challenging projects that gives me a chance to use what I have learned as well as diving into new technologies. I developed application that utilizes technologies such as Ruby on Rails, React-Redux, React Native and different databases. Some of these projects are shown below.</p>
+          <p>I enjoy working on challenging projects that gives me a chance to use what I have learned as well as diving into new technologies. I have developed applications that utilizes technologies such as Ruby on Rails, React-Redux, React Native and different databases. Some of these projects are shown below.</p>
         </div>
       </div>
     );
