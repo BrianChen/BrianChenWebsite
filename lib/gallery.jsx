ba871e2445b11273271479317702ff2b4f679f45
brian <brianc.cb@gmail.com>
2017-04-02 02:32:49 -0400

    added gallery modal

--- lib/gallery.jsx
+++ lib/gallery.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
+import Lightbox from 'react-images';
 
 class Gallery extends React.Component {
 
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.state = {
+      lightboxIsOpen: false,
+      currentImageNumber: 0
+    }
   }
 
-  handleClick() {
-    function myFunction() {
-      console.log(this);
-    }
-    myFunction();
+  handleClick(currentImageNumber) {
+    this.setState({
+      lightboxIsOpen: true,
+      currentImageNumber: currentImageNumber
+    });
   }
 
   setImages() {
     let imgs = [];
 
-    for (let i=1; i < 11; i++) {
+    for (let i=0; i < 10; i++) {
       let currentSrc = `images/picture${i}.jpg`;
-      imgs.push(<img className="gallery-img" src={currentSrc} key={currentSrc}></img>);
+      imgs.push(<img className="gallery-img" src={currentSrc} onClick={this.handleClick.bind(this, i)} key={i}></img>);
     }
     console.log(imgs);
     return imgs;
@@ -32,8 +38,29 @@ class Gallery extends React.Component {
         <div className="imgs-container">
           {this.setImages()}
         </div>
+        <Lightbox
+          images={[{src:'images/picture0.jpg'}, {src:'images/picture1.jpg'}, {src:'images/picture2.jpg'}, {src:'images/picture3.jpg'}, {src:'images/picture4.jpg'}, {src:'images/picture5.jpg'}, {src:'images/picture6.jpg'}, {src:'images/picture7.jpg'}, {src:'images/picture8.jpg'}, {src:'images/picture9.jpg'}]}
+          isOpen={this.state.lightboxIsOpen}
+          onClickPrev={this.gotoPrevious}
+          onClickNext={this.gotoNext}
+          onClose={this.closeLightbox}
+          currentImage={this.state.currentImageNumber}
+          backdropClosesModal={true}
+        />
       </div>
     );
+  }
+
+  gotoPrevious() {
+
+  }
+
+  gotoNext() {
+
+  }
+
+  closeLightbox() {
+    this.setState({lightboxIsOpen: false});
   }
 }
 
